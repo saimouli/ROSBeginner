c2f345e91cd978ec9bc44f8b896a9638adbc2fe0
Sai <ksaimouli@gmail.com>
2017-05-24 07:01:06 -0700

    Update solution.py

--- Pub&Sub/project1_solution/scripts/solution.py
+++ Pub&Sub/project1_solution/scripts/solution.py
@@ -7,14 +7,14 @@ from project1_solution.msg import TwoInts
 # write a node to subscribe topics from two_int_talker
 
 def callback(Int16): # need to publish a topic sum
-	sumab= Int16.a+Int16.b
-	pub.publish(sumab)
-	print (sumab)
+	sumab= Int16.a+Int16.b # add the subscribed two integers
+	pub.publish(sumab) #publish the sum of instegers as a sum topic 
+	print (sumab) #print to screen
 
 
 if __name__ == '__main__':
 
-	rospy.init_node('listener', anonymous=True)
+	rospy.init_node('listener', anonymous=True) #initialize subscriber as listner 
 
  	rospy.Subscriber("two_ints",TwoInts,callback)
  	pub = rospy.Publisher('sum', Int16, queue_size=10)
