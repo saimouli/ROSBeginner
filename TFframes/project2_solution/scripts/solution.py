34dccfc83f624d160d071abe276531fb98b70d6a
ksaimouli <ksaimouli@gmail.com>
2017-05-26 12:55:07 -0700

    project on TF library

---
+++ TFframes/project2_solution/scripts/solution.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python  
+import rospy
+
+import numpy as np
+
+import transformations
+
+import tf
+import tf2_ros
+import geometry_msgs.msg
+
+def publish_transforms():
+    object_transform = geometry_msgs.msg.TransformStamped()
+    object_transform.header.stamp = rospy.Time.now()
+    object_transform.header.frame_id = "base_frame"
+    object_transform.child_frame_id = "object_frame"
+    #(0.79,0,0.79) as rotations and t= (0,1,1) as translation
+    q1= tf.transformations.quaternion_from_euler(0.79,0.0,0.79) #convert roll, pitch and yaw to quat
+    object_transform.transform.rotation.x = q1[0] # convert quat. to quat. rotation matrix
+    object_transform.transform.rotation.y = q1[1]
+    object_transform.transform.rotation.z = q1[2]
+    object_transform.transform.rotation.w = q1[3]
+    object_transform.transform.translation.x= 0.0
+    object_transform.transform.translation.y= 1.0
+    object_transform.transform.translation.z= 1.0
+    br.sendTransform(object_transform)
+
+    robot_transform = geometry_msgs.msg.TransformStamped()
+    robot_transform.header.stamp = rospy.Time.now()
+    robot_transform.header.frame_id = "base_frame" #from base to robot
+    robot_transform.child_frame_id = "robot_frame"
+    # rotation around z-axis by 1.5 rad, t= (0,-1,0)
+    q2= tf.transformations.quaternion_about_axis(1.5,(0,0,1))
+    robot_transform.transform.rotation.x =q2[0]
+    robot_transform.transform.rotation.y =q2[1]
+    robot_transform.transform.rotation.z =q2[2]
+    robot_transform.transform.rotation.w =q2[3]
+    robot_transform.transform.translation.x = 0.0
+    robot_transform.transform.translation.y = -1.0
+    robot_transform.transform.translation.z = 0.0
+    br.sendTransform(robot_transform)
+ 
+    camera_transform = geometry_msgs.msg.TransformStamped()
+    camera_transform.header.stamp = rospy.Time.now()
+    camera_transform.header.frame_id = "robot_frame" #from robot to camera
+    camera_transform.child_frame_id = "camera_frame"
+    #translate (0,0.1,0.1)
+    camera_transform.transform.translation.x = 0.0
+    camera_transform.transform.translation.y = 0.1
+    camera_transform.transform.translation.z = 0.1
+    q3= tf.transformations.quaternion_about_axis(0,(0,0,0))
+    
+    #calculations to get T from camera to object
+    iq3= transformations.quaternion_inverse(q3)
+    iq2= transformations.quaternion_inverse(q2)
+    iq32= transformations.quaternion_multiply(iq3,iq2)
+    q_co= transformations.quaternion_multiply(iq32,q1)
+    rot= transformations.quaternion_matrix(q_co)
+    #rot= transformations.quaternion_matrix(q_co)
+    camera_transform.transform.rotation.x = q_co[0]#q3[0]
+    camera_transform.transform.rotation.y = q_co[1]
+    camera_transform.transform.rotation.z = q_co[2]
+    camera_transform.transform.rotation.w = q_c0[3]
+    br.sendTransform(camera_transform)
+    
+
+if __name__ == '__main__':
+    rospy.init_node('project2_solution')
+
+    br = tf2_ros.TransformBroadcaster()
+    rospy.sleep(0.5)
+
+    while not rospy.is_shutdown():
+        publish_transforms()
+        rospy.sleep(0.05)
